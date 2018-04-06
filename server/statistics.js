95119d23724dd40e3b9ce94e267b5d7cbfd7e948
Mariya Lysenkova <mariya@rampageconsulting.com>
2018-04-06 16:23:02 +0200

    Remove unnecessary parsefloat

--- server/statistics.js
+++ server/statistics.js
@@ -1,6 +1,6 @@
 module.exports = function (seq) {
   const countInstances = (regex) => (seq.match(regex) || []).length
-  const calculatePercentage = (regex) => parseFloat(100 * countInstances(regex) / seq.length).toFixed(2)
+  const calculatePercentage = (regex) => (100 * countInstances(regex) / seq.length).toFixed(2)
 
   let A = calculatePercentage(/A/g)
   let C = calculatePercentage(/C/g)
