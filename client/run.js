8d1673b4f3706fa40a55244785aa0c4218fe0c87
Mariya Lysenkova <mariya@rampageconsulting.com>
2018-04-07 09:38:34 +0200

    Remove self-explanatory comments

--- client/run.js
+++ client/run.js
@@ -10,14 +10,12 @@ module.exports = (species, region) => {
     return promise.reject('missing required argument `region`')
   }
 
-  // Ensure region is correctly formatted.
   if (!ensembl.validateRegion(region)) {
     return promise.reject('invalid format for `region` (substring definition)')
   }
 
   return ensembl.fetchRegion(species, region)
     .then(data => {
-      // If we have gotten the data, we're good to go.
       return promise.resolve(data.seq)
     })
     .catch(err => {
